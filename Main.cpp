93f96b9fddaf19d0ef5fbaec5c6b71442d3b50dc
zoho <kannan11132@gmail.com>
2022-09-27 16:02:15 +0530

    I/O from user

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,5 @@
-#include <iostream>
-#include "ValidIOHandlers.h"
-#include "Remainder.h"
+#include "Operations.h"
 
 int main() {
-	/*
-	ValidIOHandlers* validIOhandlers = new ValidIOHandlers();
-
-	string choice = validIOhandlers->getHourMinute();
-	cout << boolalpha;
-	cout << "Choice is: " << choice;
-	*/
-
-	Remainder* remainder = new Remainder();
-
-	// remainder->createEvent();
-	// remainder->readEvent();
-	// remainder->deleteEvent();
-	remainder->updateEvent();
+	new Operations;
 }
